e6610ecccf0f9d1fbc0255c084a0fefc8a0e9042
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-17 11:22:37 -0400

    I have the ball moving down and right. staying within the boundaries of the game

--- script.js
+++ script.js
@@ -10,28 +10,51 @@ const PADDLE_WIDTH = 20;
 const BALL_SIZE = 20;
 
 // Get the computer paddle element
-const computerPaddle = document.querySelector('.computer-paddle');
+const computerPaddle = document.querySelector(".computer-paddle");
+
+// Get the Ball paddle element
+const ball = document.querySelector(".ball");
 
 // Initial computer paddle y-position and y-velocity
 let computerPaddleYPosition = 0;
 let computerPaddleYVelocity = 1;
 
+// Initial BALL Y position and Velocity
+let ballYPosition = 0;
+let ballYVelocity = 1;
+
+// Initial BALL X position and Velocity
+let ballXPosition = 0;
+let ballXVelocity = 1;
+
 // Update the pong world
 function update() {
+	// Update the computer paddle's position
+	computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
-    // Update the computer paddle's position
-    computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
+	// Update the BALL 's position
+	ballYPosition = ballYPosition + ballYVelocity;
+	ballXPosition = ballXPosition + ballXVelocity;
 
-    // If the computer paddle goes off the edge of the screen, bring it back
-    computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
+	// If the computer paddle goes off the edge of the screen, bring it back
+	computerPaddleYPosition =
+		computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
 
-    // Apply the y-position 
-    computerPaddle.style.top = `${computerPaddleYPosition}px`;
+	// If the computer paddle goes off the edge of the screen, bring it back
+	ballYPosition = ballYPosition % (GAME_AREA_HEIGHT - BALL_SIZE);
+	ballXPosition = ballXPosition % (GAME_AREA_WIDTH - BALL_SIZE);
+
+	// Apply the y-position
+	computerPaddle.style.top = `${computerPaddleYPosition}px`;
+
+	// Apply the BALL position
+	ball.style.top = `${ballYPosition}px`;
+	ball.style.left = `${ballXPosition}px`;
 }
 
 // Call the update() function everytime the browser is ready to re-render
 function loop() {
-    update();
-    window.requestAnimationFrame(loop);
+	update();
+	window.requestAnimationFrame(loop);
 }
 window.requestAnimationFrame(loop);
